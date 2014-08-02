b4b346eb1b74f2fca43399e117e0320ec19dfb3d
saquib2527 <saquib2527@yahoo.com>
2014-08-02 21:55:10 +0600

    helper functions added

---
+++ app/libraries/Helper.php
@@ -0,0 +1,78 @@
+<?php
+
+class Helper{
+
+	/**
+	 * @desc performs set minus of second arg from first
+	 * @param a Array
+	 * @param b Array
+	 * @return Array subset of a not in b
+	 */
+	public static function better_array_diff($a, $b)
+	{
+		$map = [];
+		foreach($a as $val) $map[$val] = 1;
+		foreach($b as $val){
+			if(isset($map[$val])){
+				unset($map[$val]);
+			}
+		}
+		return array_keys($map);
+	}
+
+	/**
+	 * @desc returns table name from category name
+	 * @param category_name String name of category
+	 * @return String table name
+	 */
+	public static function category_name_to_table_name($category_name)
+	{
+		$table_name = strtolower(str_replace(' ', '_', $category_name));
+		$last_letter = $table_name[strlen($table_name) - 1];
+		switch($last_letter){
+			case 'y':
+				$table_name = substr($table_name, 0, -1) . 'ies';
+				break;
+			case 's':
+				break;
+			default:
+				$table_name = $table_name . 's';
+		}
+		return $table_name;
+	}
+
+	/**
+	 * @desc returns table name from category id
+	 * @param id Integer
+	 * @return String table name
+	 */
+	public static function category_id_to_table_name($id)
+	{
+		$category_name = DB::table('categories')
+						->where('id', $id)
+						->pluck('name');
+		return self::category_name_to_table_name($category_name);
+	}
+
+	/**
+	 * @desc creates table from category name
+	 * @param category_name String name of category
+	 * @return Integer 0 if table already exists, 1 otherwise
+	 */
+	public static function create_table_from_category_name($category_name)
+	{
+		$table_name = self::category_name_to_table_name($category_name);
+		if(Schema::hasTable($table_name)) return 0;
+		Schema::create($table_name, function($table){
+			$table->increments('id');
+			$table->string('question');
+			$table->char('opt1', 20);
+			$table->char('opt2', 20);
+			$table->char('opt3', 20);
+			$table->char('opt4', 20);
+			$table->tinyInteger('answer');
+		});
+		return 1;
+	}
+
+}
---
+++ dbsetup.sql
@@ -0,0 +1,124 @@
+DROP DATABASE IF EXISTS elearning;
+
+CREATE DATABASE elearning;
+
+USE elearning;
+
+CREATE TABLE users(
+	id				INTEGER(10) UNSIGNED		AUTO_INCREMENT		PRIMARY KEY,
+	fname			VARCHAR(25),
+	lname			VARCHAR(25),
+	email			VARCHAR(30) UNIQUE,
+	password		CHAR(60),
+	type			CHAR(1),
+	remember_token	CHAR(100),
+	points			INTEGER(10)
+);
+
+CREATE TABLE categories(
+	id				INTEGER(10) UNSIGNED		AUTO_INCREMENT		PRIMARY KEY,
+	name			CHAR(50),
+	description		CHAR(255)
+);
+
+CREATE TABLE sports(
+	id				INTEGER(10) UNSIGNED		AUTO_INCREMENT		PRIMARY KEY,
+	question		CHAR(255),
+	opt1			CHAR(50),
+	opt2			CHAR(50),
+	opt3			CHAR(50),
+	opt4			CHAR(50),
+	answer			TINYINT(1)
+);
+
+CREATE TABLE bangladeshs(
+	id				INTEGER(10) UNSIGNED		AUTO_INCREMENT		PRIMARY KEY,
+	question		CHAR(255),
+	opt1			CHAR(50),
+	opt2			CHAR(50),
+	opt3			CHAR(50),
+	opt4			CHAR(50),
+	answer			TINYINT(1)
+);
+
+
+CREATE TABLE current_affairs(
+	id				INTEGER(10) UNSIGNED		AUTO_INCREMENT		PRIMARY KEY,
+	question		CHAR(255),
+	opt1			CHAR(50),
+	opt2			CHAR(50),
+	opt3			CHAR(50),
+	opt4			CHAR(50),
+	answer			TINYINT(1)
+);
+
+
+CREATE TABLE geographies(
+	id				INTEGER(10) UNSIGNED		AUTO_INCREMENT		PRIMARY KEY,
+	question		CHAR(255),
+	opt1			CHAR(50),
+	opt2			CHAR(50),
+	opt3			CHAR(50),
+	opt4			CHAR(50),
+	answer			TINYINT(1)
+);
+
+
+CREATE TABLE internationals(
+	id				INTEGER(10) UNSIGNED		AUTO_INCREMENT		PRIMARY KEY,
+	question		CHAR(255),
+	opt1			CHAR(50),
+	opt2			CHAR(50),
+	opt3			CHAR(50),
+	opt4			CHAR(50),
+	answer			TINYINT(1)
+);
+
+CREATE TABLE seen(
+	user_id			INTEGER(10) UNSIGNED,
+	category_id		INTEGER(10) UNSIGNED,
+	qids			TEXT,
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (category_id) REFERENCES categories(id),
+	PRIMARY KEY (user_id, category_id)
+);
+
+-- database seeds --
+INSERT INTO users VALUES(
+	1,
+	'Nazmus',
+	'Saquib',
+	'saquib2527@yahoo.com',
+	'$2y$10$INpSUl4Jcy9AeNNITReUYO7nTShTN1grckzX58ytgh/i7rzE0gfta',
+	'M',
+	'',
+	0
+);
+
+INSERT INTO categories VALUES(
+	1,
+	'sports',
+	'a few basics on sports and stuff'
+);
+INSERT INTO categories VALUES(
+	2,
+	'bangladesh',
+	'things related to Bangladesh'
+);
+INSERT INTO categories VALUES(
+	3,
+	'current affairs',
+	'what\'s happening around'
+);
+INSERT INTO categories VALUES(
+	4,
+	'geography',
+	'know about the world'
+);
+INSERT INTO categories VALUES(
+	5,
+	'international',
+	'know about world politics'
+);
+
+
